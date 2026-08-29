9ee1634ddcb0f1ced671cb110d723e87ba8c97d6
ekariashoesanti-alt <ekariashoesanti@gmail.com>
2026-08-29 09:20:12 +0700

    Bump cache versi modul Stok setelah perubahan form

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 (function () {
   'use strict';
   var CORE = 'https://cdn.jsdelivr.net/gh/ekariashoesanti-alt/Hasnaria-ice-cream@864e0349d18a56ef997216a89661deacf9a8c24a/app.js';
-  var STOCK = '/stock-monitor.js?v=3';
+  var STOCK = '/stock-monitor.js?v=4';
   function load(src, done) {
     var s = document.createElement('script');
     s.src = src;
