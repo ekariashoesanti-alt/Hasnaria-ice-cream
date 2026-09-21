68bd5c744be140a02892d82fdc73f2bca1b6e9ea
ekariashoesanti-alt <ekariashoesanti@gmail.com>
2026-09-21 11:47:00 +0700

    db(finance): harden P3 HPP views as security invoker

---
+++ supabase/migrations/20260921044700_finance_sak_emkm_p3_security_invoker_views.sql
@@ -0,0 +1,5 @@
+alter view public.finance_hpp_period_coverage_v1 set (security_invoker=true);
+alter view public.finance_hpp_product_readiness_v1 set (security_invoker=true);
+alter view public.finance_hpp_blocker_queue_v1 set (security_invoker=true);
+alter view public.finance_hpp_direct_match_candidates_v1 set (security_invoker=true);
+alter view public.finance_close_readiness_v1 set (security_invoker=true);
